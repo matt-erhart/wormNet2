f54242a978b81b28a56b833ef7df8abf7568dc18
Matt Erhart <mattjerhart@gmail.com>
2017-09-03 18:43:42 -0700

    render propagations on gpu. framerate goes from 3 to 20-25

--- src/organizeData.js
+++ src/organizeData.js
@@ -1,26 +1,16 @@
 const _ = require("lodash");
 const d3 = require("d3");
 
-module.exports = loadData = (json, plotWidth, plotHeight) => {
-    return scaleNeuronPositions(json.neurons, plotWidth, plotHeight)
-}
+getSourceAndTargetNeurons = (neurons, sourceId, targetId) => {
+  const source = _.find(neurons, neuron => neuron.id === sourceId);
+  const target = _.find(neurons, neuron => neuron.id === targetId);
+  if (!source || !target) debugger;
+  return { source, target };
+};
 
-prepareData = json => {
-  let neuronData = json.neurons;
-  const links = json.links;
-  const propagations = json.propagations;
-
-  const { neurons } = scaleNeuronPositions(neuronData, svgWidth, svgHeight);
-  this.setState({ neurons });
-
-  neurons.forEach(neuron => {
-    const nSpikes = neuron.spikeTimes.length;
-    const nProps = _.filter(propagations, p => p.source.id === neuron.id);
-  });
-
-  this.setState({ plotMeta });
+linkPositions = (links, neurons) => {
   const scaledLinks = links.map(link => {
-    const { source, target } = this.getSourceAndTargetNeurons(
+    const { source, target } = getSourceAndTargetNeurons(
       neurons,
       link.source.id,
       link.target.id
@@ -31,43 +21,52 @@ prepareData = json => {
     const ty = target.posScaled[1];
     return { sx, sy, tx, ty, id: link.id };
   });
-  this.setState({ links: scaledLinks });
-  this.setTime(this.state.time);
 };
 
-activationLocations = (propagations, time) => {
-  if (!propagations || !time) return;
+module.exports = propagationsAsArrays = (propagations, neurons) => {
+  let propagationSources = [];
+  let propagationTargets = [];
+  let startEndTimes = [];
 
-  let propagationsOnScreen = this.state.propagations.filter(p => {
-    return (
-      _.get(p, "target.activationTime") >= time &&
-      _.get(p, "source.activationTime") < time
+  propagations.forEach((propagation, i) => {
+    const { source, target } = getSourceAndTargetNeurons(
+      neurons,
+      propagation.source.id,
+      propagation.target.id
     );
+    propagationSources[i] = source.posScaled;
+    propagationTargets[i] = target.posScaled;
+    startEndTimes[i] = [propagation.source.activationTime, propagation.target.activationTime];
   });
-  propagationsOnScreen.forEach((p, i) => {
-    const progress =
-      (time - p.source.activationTime) /
-      (p.target.activationTime - p.source.activationTime);
-    const { source, target } = this.getSourceAndTargetNeurons(
-      this.state.neurons,
-      p.source.id,
-      p.target.id
-    );
-
-    const pos = d3.interpolateObject(source.posScaled, target.posScaled)(
-      progress
-    );
-    propagationsOnScreen[i].pos = { current: pos, source: source.posScaled };
-    propagationsOnScreen[i].type = source.type;
-    propagationsOnScreen[i].id = source.id + "-" + target.id;
-  });
-
-  this.setState({ propagationsOnScreen });
+  return {propagationSources, propagationTargets, startEndTimes}
 };
 
-getSourceAndTargetNeurons = (neurons, sourceId, targetId) => {
-  const source = _.find(neurons, neuron => neuron.id === sourceId);
-  const target = _.find(neurons, neuron => neuron.id === targetId);
-  if (!source || !target) debugger;
-  return { source, target };
-};
+// activationLocations = (propagations, time) => {
+//   if (!propagations || !time) return;
+
+//   let propagationsOnScreen = this.state.propagations.filter(p => {
+//     return (
+//       _.get(p, "target.activationTime") >= time &&
+//       _.get(p, "source.activationTime") < time
+//     );
+//   });
+//   propagationsOnScreen.forEach((p, i) => {
+//     const progress =
+//       (time - p.source.activationTime) /
+//       (p.target.activationTime - p.source.activationTime);
+//     const { source, target } = this.getSourceAndTargetNeurons(
+//       this.state.neurons,
+//       p.source.id,
+//       p.target.id
+//     );
+
+//     const pos = d3.interpolateObject(source.posScaled, target.posScaled)(
+//       progress
+//     );
+//     propagationsOnScreen[i].pos = { current: pos, source: source.posScaled };
+//     propagationsOnScreen[i].type = source.type;
+//     propagationsOnScreen[i].id = source.id + "-" + target.id;
+//   });
+
+//   this.setState({ propagationsOnScreen });
+// };
